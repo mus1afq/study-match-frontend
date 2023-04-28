0b6b6fe752222c1e68fa4877680882d57a9c6c1f
mus1afq <mustafaashk@yahoo.com>
2023-04-28 18:16:21 +0100

    feat: added navbar component to main page

--- components/Navbar2.js
+++ components/Navbar2.js
@@ -2,7 +2,7 @@ import { Box, Flex, Text } from "@chakra-ui/react";
 
 export default function Navbar2() {
   return (
-    <Box bg="D1BEF1" px={4} py={2}>
+    <Box bg="#D1BEF1" px={4} py={2}>
       <Flex justify="space-between" align="center">
         <Text as="abbr" fontSize="25px" color="white">
           StudyMatch
--- pages/Main.js
+++ pages/Main.js
@@ -2,11 +2,13 @@ import CreateCard from "@/components/main/CreateCard";
 import Footer from "@/components/main/Footer";
 import JoinCard from "@/components/main/JoinCard";
 import Stats from "@/components/main/Stats";
+import Navbar2 from "@/components/Navbar2";
 import { Container, Flex } from "@chakra-ui/react";
 
 export default function Main() {
   return (
     <>
+      <Navbar2 />
       <Container maxW={"5xl"} py={12}>
         <Stats />
         <Flex>
